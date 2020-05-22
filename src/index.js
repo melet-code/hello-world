3afa35d324689d4825264d7be26dd671dc27c1a8
Maxime MELET <m.melet@local.cat-amania.com>
2020-05-22 15:11:31 +0200

    initial project version

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+const baseUrl=process.env.REACT_APP_API_URL;
+
+export const login= ( user ) => {
+  return fetch ( baseUrl + '/login' , {
+    method:'POST',
+    headers:{ 
+      'Accept':'application/json',
+      'Content-Type':'application/json'
+    },    
+    body:JSON.stringify(user)
+  }).then (res => res.json())
+}
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -10,6 +23,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
